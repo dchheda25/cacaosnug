773e299b7b5391324263c3e13e15bb15154f3098
dchheda <dc251999@gmail.com>
2020-02-27 19:36:58 -0500

    first commit

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
+function openNav() {
+  document.getElementById("sidebar").style.width = "250px";
+  document.getElementById("hamburger-menu").style.marginLeft = "250px";
+  document.getElementById("openbutton").style.display = "none";
+}
 
-
-
-        
- function showMenu() {
-     
-    
-     var menuElement = document.getElementById("myLinks");
-    
-     console.log(menuElement)
-     if (menuElement.style.display === "block") {
-        menuElement.style.display = "none";
-    } else {
-        menuElement.style.display = "block";
-    }
+function closeNav() {
+  document.getElementById("sidebar").style.width = "0";
+  document.getElementById("hamburger-menu").style.marginLeft= "0";
+  document.getElementById("openbutton").style.display = "block";
 }
